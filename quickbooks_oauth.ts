15f3b2a334a3b00eb30f8d810f5b0ccf1e41f15d
Sumitsainits <69029824+Sumitsainits@users.noreply.github.com>
2020-09-17 15:26:08 +0530

    remaining integration

--- quickbooks_oauth.ts
+++ quickbooks_oauth.ts
@@ -6,9 +6,6 @@ const OAuthClient = require('intuit-oauth');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const QuickBooks = require('node-quickbooks');
-//var buffer = require('buffer/').Buffer;
-// var request = require("request");
-
 
 const PORT = 5000;
 
@@ -24,6 +21,7 @@ app.use(express.json({
 var oauthClient = null;
 var callbackParams = null;
 var qbo = null;
+var account, bill, vendor, customer, invoice, payment, product;
 
 const readFile = (req, response) => {
     fs.readFile('./index.html', (err, html) => {
@@ -93,7 +91,7 @@ app.get('/refreshToken', (req, res) => {
 
 });
 
-// the bleow block is used to disconnecting app from quickbooks
+// the below block is used to disconnecting app from quickbooks
 // although it is revoking the connection(have to connect app again with quickbooks) but always returning error in promise
 
 app.get('/revokeToken', (req, res) => {
@@ -109,35 +107,21 @@ app.get('/revokeToken', (req, res) => {
 
 // quickbooks integration steps
 
-
-
-
 // accounts
-
-// app.get('/getAccounts', (req, res) => {
-//     qbo.findAccounts({
-//         AccountType: req.query.type, // Banking,Expenses,sales,
-//         desc: 'MetaData.LastUpdatedTime',
-//         limit: 5,
-//         offset: 5
-//     }, function (err, accounts) {
-//         if (err) console.log(err)
-//         var arr = [];
-//         console.log
-//         accounts.QueryResponse.Account.forEach(function (account) {
-//             console.log(account.Name)
-//             arr = [...arr, account]
-//         })
-
-//         res.send({ data: arr })
-//     })
-// });
 
 app.get('/getAccounts', (req, res) => {
     qbo.findAccounts({ fetchAll: true },
         function (e, allaccounts) {
             console.log(allaccounts);
-            res.send(allaccounts)
+            account = allaccounts.QueryResponse.Account;
+            if (req.query.id) {
+                let temp = account.filter(arr => {
+                    return arr.Id == req.query.id //77
+                })
+                temp.length == 0 ? res.send({ error: "Account with id not found" }) : res.send({ data: temp });
+                return
+            }
+            res.send(account)
         })
 })
 
@@ -153,67 +137,119 @@ app.get('/getBills', (req, res) => {
     qbo.findBills({ fetchAll: true },
         function (e, allbills) {
             console.log(allbills);
-            res.send(allbills)
+            bill = allbills.QueryResponse.Bill;
+            if (req.query.id) {
+                let temp = bill.filter(arr => {
+                    return arr.Id == req.query.id //77
+                })
+                temp.length == 0 ? res.send({ error: "Bill with id not found" }) : res.send({ data: temp });
+                return
+            }
+            res.send(bill)
         })
 })
 
 // vendors
 
-app.get('/getVendorbyid', (req, res) => {
-    qbo.getVendor(req.query.id, callback => {
-        console.log(callback);
-    })
-})
-
+// qbo.createVendor(object, callback)
 
 app.get('/allVendors', (req, res) => {
     qbo.findVendors({ fetchAll: true },
         function (e, allVendors) {
             console.log(allVendors)
-            res.send(allVendors)
+            vendor = allVendors.QueryResponse.Vendor;
+            if (req.query.id) {
+                let temp = vendor.filter(arr => {
+                    return arr.Id == req.query.id //77
+                })
+                temp.length == 0 ? res.send({ error: "Vendor with id not found" }) : res.send({ data: temp });
+                return
+            }
+            res.send(vendor)
         })
 })
 
 // customers
 
+// qbo.createCustomer(object, callback);
+
+// qbo.updateCustomer(object, callback)
+
 app.get('/allCustomers', (req, res) => {
     qbo.findCustomers({
         fetchAll: true
-    }, function (e, customers) {
-        console.log(customers)
-        res.send(customers)
+    }, function (e, allCustomers) {
+        console.log(allCustomers)
+        customer = allCustomers.QueryResponse.Customer;
+        if (req.query.id) {
+            let temp = customer.filter(arr => {
+                return arr.Id == req.query.id //77
+            })
+            temp.length == 0 ? res.send({ error: "Customer with id not found" }) : res.send({ data: temp });
+            return
+        }
+        res.send(customer)
     })
 });
 
 // payment 
 
+// qbo.createPayment(object, callback)
+
 app.get('/allPayments', (req, res) => {
-    qbo.findCustomers({
+    qbo.findPayments({
         fetchAll: true
-    }, function (e, payments) {
-        console.log(payments)
-        res.send(payments)
+    }, function (e, allPayments) {
+        console.log(allPayments);
+        payment = allPayments.QueryResponse.Payment;
+        if (req.query.id) {
+            let temp = payment.filter(arr => {
+                return arr.Id == req.query.id //77
+            })
+            temp.length == 0 ? res.send({ error: "Payment with id not found" }) : res.send({ data: temp });
+            return
+        }
+        res.send(payment)
     })
 });
 
-// product
+// product couldn't found any method releated to products but instead found item instead
 
-app.get('/allProducts', (req, res) => {
-    qbo.findCustomers({
-        fetchAll: true
-    }, function (e, products) {
-        console.log(products)
-        res.send(products)
-    })
-});
+// app.get('/allProducts', (req, res) => {
+//     qbo.findItems({
+//         fetchAll: true
+//     }, function (e, allProducts) {
+//         console.log(allProducts);
+//         product = allProducts.QueryResponse.Item;
+//         if (req.query.id) {
+//             let temp = product.filter(arr => {
+//                 return arr.Id == req.query.id //77
+//             })
+//             temp.length == 0 ? res.send({ error: "Product with id not found" }) : res.send({ data: temp });
+//             return
+//         }
+//         res.send(product)
+//     })
+// });
 
 //  invoice
 
+// qbo.createInvoice(object, callback)
+// qbo.sendInvoicePdf(id, sendTo, callback);   sendTo - (optinal otherwise it will be sent to email)
+
 app.get('/allInvoice', (req, res) => {
-    qbo.findCustomers({
+    qbo.findInvoices({
         fetchAll: true
-    }, function (e, invoice) {
-        console.log(invoice);
+    }, function (e, allInvoice) {
+        console.log(allInvoice);
+        invoice = allInvoice.QueryResponse.Invoice;
+        if (req.query.id) {
+            let temp = invoice.filter(arr => {
+                return arr.Id == req.query.id //77
+            })
+            temp.length == 0 ? res.send({ error: "Invoice with id not found" }) : res.send({ data: temp });
+            return
+        }
         res.send(invoice)
     })
 });
